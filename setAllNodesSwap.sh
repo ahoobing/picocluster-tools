381f01d1fec676547efec8a63d04325a64778691
ahoobing <andyh@northendlabs.com>
2021-10-10 12:43:19 -0600

    Updates.

--- setAllNodesSwap.sh
+++ setAllNodesSwap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 for x in 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0
+#for x in 19
 do
 echo "changing swap on pc$x"
-ssh pc$x '/setSwapMemorySize.sh -g 8; sudo reboot'
+ssh pc$x '~/setSwapMemorySize.sh -g 8; sudo reboot'
 done
--- setKubernetesWorkerAllNodes.sh
+++ setKubernetesWorkerAllNodes.sh
@@ -2,5 +2,5 @@
 for x in 19 18 17 16 9 8 7 6 
 do
 echo "setting kubernetes worker  pc$x"
-ssh pc$x 'sudo apt-get -y install curl; sudo curl -sfL https://get.k3s.io | K3S_URL=https://pc0:6443 K3S_TOKEN=K1096c074b88468e03ae73c19b076d3ae045050fbe92dd67483a719b5700dd091a0::server:8cd5502ef671b7ba5b71a512daa8fa35 sh -'
+ssh pc$x 'sudo apt-get -y install curl; sudo curl -sfL https://get.k3s.io | K3S_URL=https://pc0:6443 K3S_TOKEN=K101ffb15d7204800b50b3aff67dfb628375dbe8bda00d3417476acdbeaa740194f::server:089314e79ebd7b14b69e172e7405b8ff sh -'
 done
